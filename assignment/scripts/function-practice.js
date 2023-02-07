7251200b6901160d37751fc2584f44b6d1af716e
Fue Lee <fueclee.12@gmail.com>
2023-02-07 17:32:28 -0600

    adjusted answer for question 2 to make it more efficient

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,10 +15,10 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return name;
+  return 'Hello, ' + name + '!';
 }
 // Remember to call the function to test
-console.log( 'Hello,', helloName( 'Dante' ) + '!');
+console.log( helloName( 'Dante' ) );
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber, secondNumber) {
